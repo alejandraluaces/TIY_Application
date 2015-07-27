93f3316c44a085fececb60f7e0817247c9722fe5
Alejandra Luaces <aluacesr@gmail.com>
2015-07-27 16:44:39 -0500

    First commit

---
+++ Gemfile
@@ -0,0 +1,14 @@
+source 'http://rubygems.org'
+
+gem "builder", "~> 3.0"
+
+gem "middleman", "~> 3.3.7"
+gem "middleman-blog", "~> 3.5.3"
+gem "middleman-livereload", "~> 3.3.0"
+gem "middleman-autoprefixer"
+gem 'image_optim', :git => 'git://github.com/toy/image_optim.git'
+gem 'image_optim_pack'
+gem "middleman-title"
+gem "middleman-syntax"
+
+gem "pry"
---
+++ config.rb
@@ -0,0 +1,42 @@
+########################################################################
+# Basics
+########################################################################
+
+set :haml, :format => :html5
+set :css_dir, 'stylesheets'
+set :images_dir, 'images'
+set :partials_dir, 'partials'
+
+page "*", layout: "layout"
+
+########################################################################
+# Output
+########################################################################
+
+activate :syntax
+activate :livereload
+
+activate :autoprefixer do |config|
+  config.browsers = ['last 2 versions',
+    'Firefox >= 25',
+    'Android >= 4',
+    'Explorer >= 9',
+    'Opera >= 15',
+    'OperaMini >= 7']
+  config.cascade  = false
+  config.inline   = true
+  end
+
+configure :build do
+
+  activate :minify_css
+
+end
+
+helpers do
+  def navbar_link_to(title, url)
+    classes = ["sideNav__link"]
+    classes << "sideNav__link--current" if current_page.url == url
+    link_to title, url, class: classes.join(' ')
+  end
+end
