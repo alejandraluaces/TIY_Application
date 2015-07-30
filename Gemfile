8bdca67d8767e989fbefc99fee80979f7ab16af4
Alejandra Luaces <aluacesr@gmail.com>
2015-07-30 10:14:51 -0500

    Activates creeper mode

--- Gemfile
+++ Gemfile
@@ -6,8 +6,7 @@ gem 'builder', '~> 3.0'
 gem 'middleman', '~> 3.3.7'
 gem 'middleman-livereload', '~> 3.3.0'
 gem 'middleman-autoprefixer'
-gem 'image_optim', :git => 'git://github.com/toy/image_optim.git'
-gem 'image_optim_pack'
+gem "middleman-google-analytics"
 
 gem 'rack-contrib'
 gem 'puma'
--- config.rb
+++ config.rb
@@ -15,21 +15,8 @@ page "*", layout: "layout"
 
 activate :livereload
 
-activate :imageoptim do |options|
-  options.manifest = true
-  options.skip_missing_workers = true
-  options.verbose = false
-  options.nice = true
-  options.threads = true
-  options.image_extensions = %w(.png .jpg .gif .svg)
-  options.advpng    = { :level => 4 }
-  options.gifsicle  = { :interlace => false }
-  options.jpegoptim = { :strip => ['all'], :max_quality => 80 }
-  options.jpegtran  = { :copy_chunks => false, :progressive => true, :jpegrescan => true }
-  options.optipng   = { :level => 6, :interlace => false }
-  options.pngcrush  = { :chunks => ['alla'], :fix => false, :brute => false }
-  options.pngout    = { :copy_chunks => false, :strategy => 0 }
-  options.svgo      = {}
+activate :google_analytics do |ga|
+  ga.tracking_id = 'UA-65774214-1'
 end
 
 activate :autoprefixer do |config|
